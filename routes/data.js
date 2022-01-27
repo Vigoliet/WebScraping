334cab34ecc8447e7bec4982b904a16446e86623
Johan <klausenjohan23@gmail.com>
2022-01-27 15:03:47 +0100

    scrapes all jobs

--- routes/data.js
+++ routes/data.js
@@ -11,40 +11,46 @@ router.get('/', function (req, res, next) {
     async function data() {
         try {
             const URL = 'https://arbetsformedlingen.se/platsbanken/annonser?q=devops&l=2:CifL_Rzy_Mku'
-            const browser = await puppeteer.launch()
+            const browser = await puppeteer.launch({headless: true})
             const page = await browser.newPage()
 
             await page.goto(URL)
-            let data = await page.evaluate(() => {
-                results = [];
-                let items = document.querySelectorAll('.card-container')
-                items.forEach((item) => {
-                    results.push({
-                    // title(job), company name and location
-                        // job
-                        title: item.querySelector('a').innerText, // takes the inner text of the a-link attribute
-                    
-                        // company name and location
-                        companyname: item.querySelector('.pb-company-name').innerText,
+            let pagesToScrape = 3;
+            let currentPage = 1;
+            let data = []
+            while (currentPage <= pagesToScrape) {
+                let newResults = await page.evaluate(() => {
+                    let results = []
+                    let items = document.querySelectorAll('.card-container')
+                    items.forEach((item) => {
+                        results.push({
+                               // job
+                             title: item.querySelector('a').innerText, // takes the inner text of the a-link attribute
+
+                         // company name and location
+                             companyname: item.querySelector('.pb-company-name').innerText,
+                        })
                     })
-                })  
-                
-                return results
-              
-            })
-
+                    return results
+                })
+                data = data.concat(newResults)
+                if (currentPage < pagesToScrape) {
+                    await page.click('.sc-digi-button-h .digi-button--icon-secondary.sc-digi-button')
+                    await page.waitForSelector('.card-container')
+                 //    await page.waitForSelector('.sc-digi-button')
+                }
+                currentPage++;
+            }
             console.log(data)
-            res.send(data);
+            res.send(data)
             await browser.close()
-
         } catch (error) {
             console.error(error)
         }
-      
-    }
-   
-    data();
 
+    }
+
+    data();
 });
 
 module.exports = router;
